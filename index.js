3dbf096c9788c26fd801f13ca0ce6b33055e473d
Ophir Bushinsky <ophirbushi@gmail.com>
2020-08-26 23:00:43 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const fetch = require('node-fetch');
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const marked = require('marked');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/blog/:blogId', async (req, res) => {
+    const post = await fetch(`http://localhost:1337/posts/${req.params.blogId}`)
+        .then(res => res.json())
+        .then(post => ({ ...post, content: marked(post.content) }))
+        .catch(err => new Error(err));
+
+    if (post instanceof Error) {
+        res.redirect('/error');
+    } else {
+        res.render('blog', { post });
+    }
+});
+
+app.get('/error', (req, res) => {
+    res.render('error');
+});
+
+app.listen(8080);
